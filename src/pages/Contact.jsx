24e2264650586471e7158c2490276861e5c947e0
Safi Khan <safikhan5890@gmail.com>
2022-08-24 14:24:35 -0700

    added inde.html file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,43 +1,42 @@
-
-
 const Contact = () => {
-    return (
-        <div id="contact">
-            <div class="announce">THE INTERNET PRESENTS</div>
-            <div class="letters">
-            <div class="bytes">k</div>
-            <div class="bytes">i</div>
-            <div class="bytes">t</div>
-            <div class="bytes">t</div>
-            <div class="bytes">y</div>
-            <div class="bytes">c</div>
-            <div class="bytes">a</div>
-            <div class="bytes">t</div>
-            <div class="bytes">b</div>
-            <div class="bytes">y</div>
-            <div class="bytes">t</div>
-            <div class="bytes">e</div>
-            <div class="bytes">s</div>
-            </div>
-            <div id="contactCards">
-                <a href="http://instagram.com/dariamayfair" target="_blank">
-                    <img width="80px" height="80px" src="/images/IG.png"></img>
-                </a>
-                <a href="mailto:cat@kittycatbytes.com" target="_blank">
-                    <img width="80px" height="80px" src="/images/email.png"></img>
-                </a>
-                <a href="http://twitter.com/kittycatbytes" target="_blank">
-                    <img width="80px" height="80px" src="/images/twitter.png"></img>
-                </a>
-                <a href="https://kittycatbytes.wordpress.com/" target="_blank">
-                    <img width="80px" height="80px" src="/images/wordpress.png" alt="Ramblings"></img>
-                </a>
-                <a href="http://linkedin.com/in/kittycatbytes" target="_blank">
-                    <img width="80px" height="80px" src="/images/linkedin.png"></img>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div id="contact">
+      <div class="announce">THE INTERNET PRESENTS</div>
+      <div class="letters">
+        <div class="bytes">s</div>
+        <div class="bytes">a</div>
+        <div class="bytes">f</div>
+        <div class="bytes">i</div>
+        <div class="bytes">u</div>
+        <div class="bytes">l</div>
+        <div class="bytes">l</div>
+        <div class="bytes">a</div>
+        <div class="bytes">h</div>
+      </div>
+      <div id="contactCards">
+        <a href="http://instagram.com/safi5890" target="_blank">
+          <img width="80px" height="80px" src="/images/IG.png"></img>
+        </a>
+        <a href="safikhan5890@gmail.com" target="_blank">
+          <img width="80px" height="80px" src="/images/email.png"></img>
+        </a>
+        <a href="http://twitter.com/safikhan5834" target="_blank">
+          <img width="80px" height="80px" src="/images/twitter.png"></img>
+        </a>
+        <a href="https://non.wordpress.com/" target="_blank">
+          <img
+            width="80px"
+            height="80px"
+            src="/images/wordpress.png"
+            alt="Ramblings"
+          ></img>
+        </a>
+        <a href="http://linkedin.com/in/safikhan5890" target="_blank">
+          <img width="80px" height="80px" src="/images/linkedin.png"></img>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
