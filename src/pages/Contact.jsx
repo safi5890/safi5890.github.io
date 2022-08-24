51bf2dfbcd9b3335bc1d017a290e4fe886fa7c02
Safi Khan <safikhan5890@gmail.com>
2022-08-24 12:50:12 -0700

    added page files

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,43 @@
+
+
+const Contact = () => {
+    return (
+        <div id="contact">
+            <div class="announce">THE INTERNET PRESENTS</div>
+            <div class="letters">
+            <div class="bytes">k</div>
+            <div class="bytes">i</div>
+            <div class="bytes">t</div>
+            <div class="bytes">t</div>
+            <div class="bytes">y</div>
+            <div class="bytes">c</div>
+            <div class="bytes">a</div>
+            <div class="bytes">t</div>
+            <div class="bytes">b</div>
+            <div class="bytes">y</div>
+            <div class="bytes">t</div>
+            <div class="bytes">e</div>
+            <div class="bytes">s</div>
+            </div>
+            <div id="contactCards">
+                <a href="http://instagram.com/dariamayfair" target="_blank">
+                    <img width="80px" height="80px" src="/images/IG.png"></img>
+                </a>
+                <a href="mailto:cat@kittycatbytes.com" target="_blank">
+                    <img width="80px" height="80px" src="/images/email.png"></img>
+                </a>
+                <a href="http://twitter.com/kittycatbytes" target="_blank">
+                    <img width="80px" height="80px" src="/images/twitter.png"></img>
+                </a>
+                <a href="https://kittycatbytes.wordpress.com/" target="_blank">
+                    <img width="80px" height="80px" src="/images/wordpress.png" alt="Ramblings"></img>
+                </a>
+                <a href="http://linkedin.com/in/kittycatbytes" target="_blank">
+                    <img width="80px" height="80px" src="/images/linkedin.png"></img>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import Canvas from "../components/homecanvas";
+import ColorPicker from "../components/color-picker";
+var index = 1;
+var visible = true;
+
+const Home = (props) => {
+  const [colorPicked1, setColorPicked1] = useState("#85BBFD");
+  const [colorPicked2, setColorPicked2] = useState("#1E6DCE");
+  const [currentQuote, setQuote] = useState(`"Be kind whenever possible. It is always possible." —Dalai Lama`);
+  var quotes = [
+    `"Be kind whenever possible. It is always possible." —Dalai Lama`,
+    `"I no doubt deserved my enemies, but I don't believe I deserved my friends." —Walt Whitman`,
+    `"People do not seem to realize that their opinion of the world is also a confession of character." —Ralph Waldo Emerson`,
+    `"Unlike physical progress, which is subject to natural restrictions, the qualities of the mind can be developed limitlessly." — Dalai Lama`,
+    `"Without music, life would be a mistake." —Friedrich Nietzsche`,
+    `"Every story I create, creates me. I write to create myself." —Octavia E. Butler`,
+    `"We can't solve problems by using the same kind of thinking we used when we created them." —Albert Einstein`,
+    `"It is not in the stars to hold our destiny but in ourselves." —William Shakespeare`,
+    `"It's not what happens to you, but how you react to it that matters." —Epictetus`,
+    `"One impulse from a vernal wood May teach you more of man, of moral evil and of good, than all the sages can." —William Wordsworth`,
+    `"A kiss that is never tasted, is forever and ever wasted." —Billie Holiday`,
+    `"He who knows, does not speak. He who speaks, does not know." —Lao Tzu`
+  ];
+
+  const shuffle = useCallback(() => {
+    index++;
+    if (index === 12) {
+      index = 0;
+    }
+    setQuote(quotes[index]);
+  }, []);
+
+  useEffect(() => {
+    const intervalID = setInterval(shuffle, 10000);
+    return () => { clearInterval(intervalID);};
+  }, [shuffle]);
+
+
+  localStorage.setItem('color1', colorPicked1);
+  localStorage.setItem('color2', colorPicked2);
+
+
+  const inputchangehandler1 = (color) => {
+    console.log(color.hex);
+    setColorPicked1(color.hex);
+    console.log("iscolorset1:" + colorPicked1);
+    localStorage.setItem('color1', colorPicked1);
+
+  }
+  const inputchangehandler2 = (color) => {
+    console.log(color.hex);
+    setColorPicked2(color.hex);
+    console.log("iscolorset2:" + colorPicked2);
+    localStorage.setItem('color2', colorPicked2);
+  }
+
+  return <div id="home">
+    <Canvas canvasColor1={colorPicked1} canvasColor2={colorPicked2}></Canvas>
+    <div id="contactCards" class="hidden">
+      <img width="80px" height="80px" src="/images/IG.png"></img>
+      <img width="80px" height="80px" src="/images/email.png"></img>
+      <img width="80px" height="80px" src="/images/twitter.png"></img>
+      <img width="80px" height="80px" src="/images/wordpress.png" alt="Ramblings"></img>
+      <img width="80px" height="80px" src="/images/linkedin.png"></img>
+      <div class="load-font bytes">abc</div>
+    </div>
+    <div id="quotes" class="fadeInOut">{currentQuote}</div>
+    <div id="colorPickers" >
+      <div><ColorPicker onColorPickerClick={inputchangehandler1} color={colorPicked1}></ColorPicker></div>
+      <div><ColorPicker onColorPickerClick={inputchangehandler2} color={colorPicked2}></ColorPicker></div>
+    </div>
+  </div>;
+};
+
+export default Home;
