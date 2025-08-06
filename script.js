0f87aa926d5197dbecc20aee14aabcc741e6ca3f
Safiullah <109414497+safi5890@users.noreply.github.com>
2025-08-06 23:42:38 +0500

    update the html file

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.box').addEventListener('animationend', () => {
-    const socialIcons = document.querySelector('.social-icons');
-    socialIcons.style.display = 'flex'; // Make icons visible
-    socialIcons.style.opacity = '1';   // Fade in the icons
-});
